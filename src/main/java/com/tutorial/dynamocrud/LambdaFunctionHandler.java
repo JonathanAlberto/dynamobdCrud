535c3f62f324963f12d3fc99e174f12d249181a5
JonathanAlberto <jona_chivas_4000@hotmail.com>
2022-01-14 19:16:16 -0600

    Se agrego clase Produc, request y se agregaron metodos http

--- src/main/java/com/tutorial/dynamocrud/LambdaFunctionHandler.java
+++ src/main/java/com/tutorial/dynamocrud/LambdaFunctionHandler.java
@@ -1,16 +1,46 @@
 package com.tutorial.dynamocrud;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+public class LambdaFunctionHandler implements RequestHandler<Request, Object> {
 
-    @Override
-    public String handleRequest(Object input, Context context) {
-        context.getLogger().log("Input: " + input);
+	@Override
+	public Object handleRequest(Request request, Context context) {
+		AmazonDynamoDB db = AmazonDynamoDBClientBuilder.defaultClient();
+		DynamoDBMapper mapper = new DynamoDBMapper(db);
+		Producto producto = null;
+		
+		switch (request.getHttpMethod()) {
+			case "GET":
+				if(request.getId() == 0) {
+					List<Producto> productos = new ArrayList<>();
+					productos = mapper.scan(Producto.class, new DynamoDBScanExpression());
+					return productos;
+				}else {
+					producto = mapper.load(Producto.class, request.getId());
+					return producto;
+				}
+			case "POST":
+				producto = request.getProducto();
+				mapper.save(Producto.class);
+				return producto;
+			case "DELETE":
+				producto = mapper.load(Producto.class, request.getId());
+				mapper.delete(producto);
+				return producto;
+				
+		}
+		return null;
+	}
 
-        // TODO: implement your handler
-        return "Hello from Lambda!";
-    }
+   
 
 }
---
+++ src/main/java/com/tutorial/dynamocrud/Producto.java
@@ -0,0 +1,58 @@
+package com.tutorial.dynamocrud;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable (tableName = "producto")
+public class Producto {
+	
+	@DynamoDBHashKey
+	private int id;
+	
+	@DynamoDBAttribute
+	private String nombre;
+	
+	@DynamoDBAttribute
+	private String precio;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+
+	public Producto(int id, String nombre, String precio) {
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
